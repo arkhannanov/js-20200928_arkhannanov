5e644b5ca24c920156ae2e617c3d39a3b886205a
Artur Khannanov <arkhannanov@gmail.com>
2020-10-10 14:55:15 +0300

    solved

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -5,4 +5,19 @@
  */
 export function createGetter(path) {
 
+  let pathArray = path.split('.');
+
+  return (obj) = {
+
+    let result = obj;
+
+  for (const item of pathArray) {
+    if (result === undefined) {
+      break;
+    }
+
+    result = result[item];
+  }
+  return result;
+}
 }
